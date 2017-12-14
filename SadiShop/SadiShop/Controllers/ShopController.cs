a58a811d25f247cd9f5f249d9903df53b9d82301
Nguyen The Thoai <thethoainguyenn@gmail.com>
2017-12-14 09:11:44 +0700

    bTrangChu Hoan thanh trang chu

--- SadiShop/SadiShop/Controllers/ShopController.cs
+++ SadiShop/SadiShop/Controllers/ShopController.cs
@@ -11,6 +11,47 @@ namespace SadiShop.Controllers
 {
     public class ShopController : Controller
     {
+		 dbQLQuanAoDataContext data = new dbQLQuanAoDataContext();
+        //--------------------------------HIỂN THỊ SẢN PHẨM TRANG INDEX--------------------------------
+        private List<SanPham> Laysanphammoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.MaSanPham).Take(count).ToList();
+        }
 		
+        // GET: Shop
+        public ActionResult Index()
+        {
+            var sanphammoi = Laysanphammoi(6);
+            return View(sanphammoi);
+        }
+		  //---------------------------LOẠI SẢN PHẨM TRÊN MENU ----------------------------------
+        public ActionResult LoaisanphamNu()
+        {
+            var loai = from l in data.LoaiSanPhams where l.MaLoaiCha == "L01" select l;
+            return PartialView(loai);
+        }
+
+        public ActionResult LoaisanphamNam()
+        {
+            var loai = from l in data.LoaiSanPhams where l.MaLoaiCha == "L02" select l;
+            return PartialView(loai);
+        }
+
+        //----------------------------------GIÀY----------------------------------------
+        public ActionResult LoaisanphamGiay()
+        {
+            var loai = from l in data.LoaiSanPhams where l.MaLoaiCha == "L03" select l;
+            return PartialView(loai);
+        }
+
+        //----------------------------------------------KHÁC--------------------------------------------
+        public ActionResult LoaisanphamPhuKien()
+        {
+            var loai = from l in data.LoaiSanPhams where l.MaLoaiCha == "L04" select l;
+            return PartialView(loai);
+        }
+
+
+
 	}
 }
