9bd31007467b6e65e0198d4bd69ab637cea5798f
Nguyễn Gia Quốc <quocq6hcm@hotmail.com>
2017-12-14 10:07:02 +0700

    bLienHe_HoanThanhTrangLienHeSaleAbout

--- SadiShop/SadiShop/Controllers/ShopController.cs
+++ SadiShop/SadiShop/Controllers/ShopController.cs
@@ -11,6 +11,31 @@ namespace SadiShop.Controllers
 {
     public class ShopController : Controller
     {
-		
+	 dbQLQuanAoDataContext data = new dbQLQuanAoDataContext();
+
+//--------------------------------SẢN PHẨM SALE--------------------------------
+        private List<SanPham> SanPhamSale(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.GiaBan).Take(count).ToList();
+        }
+
+        public ActionResult Sale()
+        {
+            var sanphamsale = SanPhamSale(10);
+            return View(sanphamsale);
+        }
+
+//-------------------------------ABOUT + LIÊN HỆ--------------------------------
+	public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+
 	}
 }
