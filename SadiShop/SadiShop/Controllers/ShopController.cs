1f3240de12ce515403be8056975407b3e09e2de0
Dinh Nguyen <nguyendinhteki@gmail.com>
2017-12-15 15:24:02 +0700

    bXemNhanh_XuHuongMoi Hoan thanh trang xem nhanh va xu huong moi

--- SadiShop/SadiShop/Controllers/ShopController.cs
+++ SadiShop/SadiShop/Controllers/ShopController.cs
@@ -11,6 +11,30 @@ namespace SadiShop.Controllers
 {
     public class ShopController : Controller
     {
+		dbQLQuanAoDataContext data = new dbQLQuanAoDataContext();
 		
+		public ActionResult XemNhanh(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return View();
+            }
+            var sanpham = data.SanPhams.SingleOrDefault(n => n.MaSanPham == id);
+            var nhasanxuat = data.NhanSanXuats.SingleOrDefault(n => n.MaNhaSanXuat == sanpham.MaNhaSanXuat);
+            ViewBag.nsx = nhasanxuat.TenNhaSanXuat;
+            //var sanpham = (from sp
+            //               in data.SanPhams
+            //               where sp.MaSanPham == id
+            //               select sp
+            //               ).SingleOrDefault();
+            return PartialView("XemNhanh", sanpham);
+			
+			//--------------------------------XU HƯỚNG MỚI--------------------------------
+        public ActionResult XuHuongMoi()
+        {
+            var sanpham = from sp in data.SanPhams orderby sp.MaSanPham ascending select sp;
+            return PartialView(sanpham);
+        }
+        }
 	}
 }
